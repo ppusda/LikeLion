8d116eff9bf22f5e3c8683516fa0d0b1d70c9c06
ppusda <ppusda1234@gmail.com>
2023-10-30 13:46:30 +0900

    feat: 사칙연산 기능 구현

--- src/main/java/org/study/operation/Calc.java
+++ src/main/java/org/study/operation/Calc.java
@@ -2,22 +2,31 @@ package org.study.operation;
 
 public class Calc {
 
-    static int run(String word) {
-        String[] tmp = word.split(" ");
-        int prefix =  Integer.parseInt(tmp[0]);
-        int postfix = Integer.parseInt(tmp[2]);
+    static int run(String exp) {
+        if(exp.isBlank()) return 0;
 
-        switch (tmp[1]){
-            case "+":
-                return prefix + postfix;
-            case "*":
-                return prefix * postfix;
-            case "-":
-                return prefix - postfix;
-            case "/":
-                return prefix /  postfix;
+        String[] expBits = exp.split(" ");
+
+        if(expBits.length > 1) {
+            String operator = expBits[1];
+            int prefix =  Integer.parseInt(expBits[0]);
+            int postfix = Integer.parseInt(expBits[2]);
+
+            return switch (operator){
+                case "+":
+                    yield prefix + postfix;
+                case "*":
+                    yield prefix * postfix;
+                case "-":
+                    yield prefix - postfix;
+                case "/":
+                    yield prefix /  postfix;
+                default:
+                    throw new IllegalStateException("Unexpected value: " + operator);
+            };
+        } else {
+            return 0;
         }
-        return -1;
     }
 }
 // word.substring(word.lastIndexOf("(")+1, word.indexOf(")")).split(" ");
--- src/test/java/org/study/operation/CalcTest.java
+++ src/test/java/org/study/operation/CalcTest.java
@@ -1,5 +1,8 @@
 package org.study.operation;
 
+import static org.assertj.core.api.Assertions.*;
+
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,4 +13,14 @@ public class CalcTest {
     void CalcReturnTest() {
         int result = Calc.run("");
     }
+
+    @Test
+    @DisplayName("Calc에 run 메서드는 알맞은 사칙연산 값을 리턴 한다.")
+    void CalcResultTest() {
+        assertThat(Calc.run("10 + 10")).isEqualTo(20);
+        assertThat(Calc.run("10 - 10")).isEqualTo(0);
+        assertThat(Calc.run("10 * 10")).isEqualTo(100);
+        assertThat(Calc.run("10 / 10")).isEqualTo(1);
+    }
+
 }
